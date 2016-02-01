28ce6ca73c1fc2d57aa6349dde616283f306006c
Felix Chapman <felix@mindmaps.io>
2016-02-01 12:32:30 +0000

    Add support for working behaviour

--- src/main/java/CreateSortEdge.java
+++ src/main/java/CreateSortEdge.java
@@ -6,11 +6,22 @@ import org.apache.tinkerpop.gremlin.structure.Direction;
 import org.apache.tinkerpop.gremlin.structure.Graph;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
 
-import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class CreateSortEdge {
-    private static Graph mainGraph;
+    private static final String[] TYPES = new String[] {
+            "http://mindmaps.io/genre",
+            "http://mindmaps.io/award",
+            "http://mindmaps.io/mood",
+            "http://mindmaps.io/location",
+            "http://mindmaps.io/language",
+            "http://mindmaps.io/person",
+            "http://mindmaps.io/character",
+            "http://mindmaps.io/company",
+            "http://mindmaps.io/certificate",
+            "http://mindmaps.io/keyword"
+    };
 
 
     public static void main(String [] args){
@@ -22,29 +33,53 @@ public class CreateSortEdge {
             return;
         }
 
-        System.out.println("Creating connection to graph . . . ");
-        mainGraph = MindmapsGraphFactory.buildNewTransaction(args[0]);
-        System.out.println();
-        System.out.println("Querying genres . . .");
-        List<Vertex> genres = mainGraph.traversal().V().has("ITEM_IDENTIFIER", "http://mindmaps.io/genre").in("ISA").order().by("DEGREE", Order.decr).toList();
-
-        System.out.println("Creating edges");
-        createdNextEdges(genres);
-
-        mainGraph.tx().commit();
-        try {
-            mainGraph.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        Stream.of(TYPES).parallel().forEach(type -> createNextEdgesForType(args[0], type));
 
         System.out.println("Done");
     }
 
-    private static void createdNextEdges(List<Vertex> vertexes){
-        Vertex currentVertex = vertexes.get(0);
-        for(int i =1; i < vertexes.size(); i ++ ){
-            Vertex nextVertex = vertexes.get(i);
+    private static void createNextEdgesForType(String config, String typeId) {
+        System.out.println(typeId);
+
+        Graph graph = MindmapsGraphFactory.buildNewTransaction(config);
+
+        List<Vertex> vertices = graph.traversal().V()
+                .has("ITEM_IDENTIFIER", typeId)
+                .in("ISA")
+                .order()
+                .by("DEGREE", Order.decr)
+                .toList();
+
+        System.out.println("Creating edges: " + typeId);
+        createNextEdges(vertices);
+
+        System.out.println("top and bottom: " + typeId);
+
+        Vertex typeVertex = graph.traversal().V().has("ITEM_IDENTIFIER", typeId).next();
+
+        if (vertices.size() > 0) {
+            if (!typeVertex.edges(Direction.OUT, "TOP").hasNext()) typeVertex.addEdge("TOP", vertices.get(0));
+            if (!typeVertex.edges(Direction.OUT, "BOTTOM").hasNext())
+                typeVertex.addEdge("BOTTOM", vertices.get(vertices.size() - 1));
+        }
+
+        graph.tx().commit();
+        try {
+            graph.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            createNextEdgesForType(config, typeId);
+        }
+
+        System.out.println("Done: " + typeId);
+    }
+
+    private static void createNextEdges(List<Vertex> vertices){
+        if (vertices.size() < 2) return;
+
+        Vertex currentVertex = vertices.get(0);
+        for(int i =1; i < vertices.size(); i ++ ){
+            Vertex nextVertex = vertices.get(i);
 
             //Check if edge exists
             if(currentVertex.edges(Direction.OUT, "NEXT").hasNext()){
